b54c8e199369be3fff1d32f492ed30c85b38dc23
Taha Abbas <taha.abbas@ideofuzion.com>
2026-02-08 16:01:30 +0500

    fix: buffer error on netlify

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,7 @@
+import { Buffer } from "buffer";
+if (typeof window !== "undefined") {
+  (window as unknown as { Buffer: typeof Buffer }).Buffer = Buffer;
+}
 import { useMemo } from "react";
 import {
   isRouteErrorResponse,
